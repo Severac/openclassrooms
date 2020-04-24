ac7a0fc523c1c4b3aaf248a705555a7c16c315eb
Severac <francois.boyer@gmail.com>
2020-04-24 18:56:40 +0200

    PJ4: ajout functions.py pour l'UI

---
+++ PJ4/functions.py
@@ -0,0 +1,496 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 24 15:45:19 2020
+
+@author: francois
+"""
+
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+
+from sklearn import decomposition
+from sklearn import preprocessing
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+import statistics
+
+from scipy import sparse
+
+import pandas as pd
+
+
+'''
+Cette fonction fait un 1 hot encoding des features qui sont des catégories
+Old function not used anymore
+'''
+    
+def add_categorical_features_1hot(df, categorical_features_totransform):
+    #df.drop(labels=categorical_features_totransform, axis=1, inplace=True)
+    
+    
+    #df_encoded = pd.get_dummies(df, columns=categorical_features_totransform, sparse=True)
+    
+    for feature_totransform in categorical_features_totransform:
+        print(f'Adding 1hot Feature : {feature_totransform}')
+        
+        print('First')
+        df_transformed = df[feature_totransform].str.get_dummies().add_prefix(feature_totransform +'_')   
+        
+        #df_new = pd.get_dummies(df, columns=['ORIGIN'])
+        
+        
+        
+        
+        #df.drop(labels=feature_totransform, axis=1, inplace=True)
+        print('Second')
+        del df[feature_totransform]
+        
+        print('Third')
+        df = pd.concat([df, df_transformed], axis=1)
+        
+    return(df)
+
+
+class HHMM_to_Minutes(BaseEstimator, TransformerMixin):
+    def __init__(self, features_toconvert = ['CRS_DEP_TIME', 'CRS_ARR_TIME']):
+        self.features_toconvert = features_toconvert
+        return None
+    
+    def fit(self, df):      
+        return self
+    
+    '''
+    def transform(self, df):
+        return(df)
+    '''
+    
+    def transform(self, df):      
+        for feature_toconvert in self.features_toconvert:
+            print(f'Converting feature {feature_toconvert}\n')
+            #print('1\n')
+            df_concat = pd.concat([df[feature_toconvert].str.slice(start=0,stop=2, step=1),df[feature_toconvert].str.slice(start=2,stop=4, step=1)], axis=1).astype(int).copy(deep=True)
+                    
+            #print('2\n')
+            df[feature_toconvert] = (df_concat.iloc[:, [0]] * 60 + df_concat.iloc[:, [1]])[feature_toconvert]
+            del df_concat
+            
+            #print('3\n')
+        
+        return(df)
+        
+    
+class HHMM_to_HH(BaseEstimator, TransformerMixin):
+    def __init__(self, features_toconvert = ['CRS_DEP_TIME', 'CRS_ARR_TIME']):
+        self.features_toconvert = features_toconvert
+        return None
+    
+    def fit(self, df):      
+        return self
+    
+    def transform(self, df):       
+        for feature_toconvert in self.features_toconvert:
+            print(f'Converting feature {feature_toconvert}\n')
+            #print('1\n')
+
+            df.loc[:, feature_toconvert] = df[feature_toconvert].str.slice(start=0,stop=2, step=1)
+        
+        return(df)
+    
+'''
+class CategoricalFeatures1HotEncoder_old(BaseEstimator, TransformerMixin):
+    def __init__(self, categorical_features_totransform=['ORIGIN', 'UNIQUE_CARRIER', 'DEST']):
+        self.categorical_features_totransform = categorical_features_totransform
+    
+    def fit(self, df, labels=None):      
+        return self
+    
+    def transform(self, df):
+        # /!\ Array will not have the same shape if we fit an ensemble of samples that have less values than total dataset
+        df_encoded = pd.get_dummies(df, columns=self.categorical_features_totransform, sparse=True)  # Sparse allows to gain memory. But then, standardscale must be with_mean=False
+        #df_encoded = pd.get_dummies(df, columns=self.categorical_features_totransform, sparse=False)
+        print('type of df : ' + str(type(df_encoded)))
+        return(df_encoded)
+'''
+
+class CategoricalFeatures1HotEncoder(BaseEstimator, TransformerMixin):
+    #def __init__(self, categorical_features_totransform=['ORIGIN', 'UNIQUE_CARRIER', 'DEST']):
+    def __init__(self):
+        #self.categorical_features_totransform = categorical_features_totransform
+        self.fitted = False
+        self.all_feature_values = {}
+        #self.df_encoded = None
+    
+    #def fit(self, df, labels=None):      
+    def fit(self, df, categorical_features_totransform=['ORIGIN', 'UNIQUE_CARRIER', 'DEST']):      
+        print('Fit data')
+        self.categorical_features_totransform = categorical_features_totransform
+        print('!! categorical_features_totransform' + str(self.categorical_features_totransform))
+
+        if (self.categorical_features_totransform != None):
+            for feature_name in self.categorical_features_totransform:
+                df[feature_name] = df[feature_name].astype(str) # Convert features to str in case they are not already     
+                self.all_feature_values[feature_name] = feature_name + '_' + df[feature_name].unique()
+        
+        self.fitted = True
+        
+        return self
+    
+    def transform(self, df):
+        if (self.fitted == False):
+            self.fit(df)
+        
+        if (self.categorical_features_totransform != None):
+            print('Transform data')
+            
+            print('1hot encode categorical features...')
+            #df_encoded = pd.get_dummies(df, columns=self.categorical_features_totransform, sparse=True)  # Sparse allows to gain memory. But then, standardscale must be with_mean=False
+            df_encoded = pd.get_dummies(df, columns=self.categorical_features_totransform, sparse=False)
+
+            # Get category values that were in fitted data, but that are not in data to transform 
+            for feature_name, feature_values in self.all_feature_values.items():
+                diff_columns = list(set(feature_values) - set(df_encoded.columns.tolist()))
+                print(f'Column values that were in fitted data but not in current data: {diff_columns}')
+
+                if (len(diff_columns) > 0):
+                    print('Adding those column with 0 values to the DataFrme...')
+                    # Create columns with 0 for the above categories, in order to preserve same matrix shape between train et test set
+                    zeros_dict = dict.fromkeys(diff_columns, 0)
+                    df_encoded = df_encoded.assign(**zeros_dict)
+
+            print('type of df : ' + str(type(df_encoded)))
+            return(df_encoded)
+
+        else:
+            return(df)
+
+class Aggregate_then_GroupByMean_then_Sort_numericalEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, categorical_features_totransform=['ORIGIN', 'UNIQUE_CARRIER', 'DEST']):
+        self.categorical_features_totransform = categorical_features_totransform
+        self.fitted = False
+        self.all_feature_values = {}
+    
+    def fit(self, df, labels=None):      
+        print('Fit data')
+        
+        self.feature_maps = {}
+        
+        for feature_name in self.categorical_features_totransform:
+            print(f'Fitting feature {feature_name}')
+            # List all feature values ordered by mean delay
+            list_feature_mean_ordered = df[['ARR_DELAY', feature_name]].groupby(feature_name).mean().sort_values(by='ARR_DELAY', ascending=True).index.tolist()
+            
+            # Generate a dictionary of feature values as keys and index as values
+            self.feature_maps[feature_name] = {}
+            self.feature_maps[feature_name]['list_feature_mean_ordered_dict'] = {list_feature_mean_ordered[i] : i for i in range(len(list_feature_mean_ordered))  }
+            
+            #print('Dictionnary : ' + str(self.feature_maps[feature_name]['list_feature_mean_ordered_dict']))
+            
+            # BUG : we had to do that line of code in transform instead, not in fit (result is the same, no difference)
+            #self.feature_maps[feature_name]['list_feature_mean_ordered_mapper'] = lambda k : self.feature_maps[feature_name]['list_feature_mean_ordered_dict'][k]
+                  
+        self.fitted = True
+        
+        return self
+    
+    def transform(self, df):
+        if (self.fitted == False):
+            print('Launching fit first (if you see this message : ensure that you have passed training set as input, not test set)')
+            self.fit(df)
+        
+        print('Encode categorical features...')
+        
+        for feature_name in self.categorical_features_totransform:
+            print(f'Encoding feature {feature_name} ...')
+            #print('Dictionnary : ' + str(self.feature_maps[feature_name]['list_feature_mean_ordered_dict']))
+            
+            # Replace each feature value by its index (the lowest the index, the lowest the mean delay is for this feature)
+            list_feature_mean_ordered_mapper = lambda k : self.feature_maps[feature_name]['list_feature_mean_ordered_dict'][k]
+                            
+            #df[feature_name] = df.loc[:, feature_name].apply(self.feature_maps[feature_name]['list_feature_mean_ordered_mapper'])  # BUG (we had to use line below instead)
+            df[feature_name] = df.loc[:, feature_name].apply(list_feature_mean_ordered_mapper)
+
+        return(df)
+    
+    
+class FeaturesSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, features_toselect = None):  # If None : every column is kept, nothing is done
+        self.features_toselect = features_toselect
+    
+    def fit(self, df, labels=None):      
+        return self
+    
+    def transform(self, df):       
+        if (self.features_toselect != None):
+            filter_cols = [col for col in df if (col.startswith(tuple(self.features_toselect)))]
+            
+            filter_cols.sort()
+            
+            print("Features selected (in order): " + str(df[filter_cols].columns))
+            
+            df = df.loc[:, filter_cols]
+            return(df)
+
+        else:
+            return(df)
+
+'''
+In order have less features globally: we Keep only features_tofilter that represent percent_tokeep% of total values
+Features which values represent less than percent_tokeep% will be set "OTHERS" value instead of their real value
+'''
+
+class Filter_High_Percentile(BaseEstimator, TransformerMixin):
+    def __init__(self, features_tofilter = ['ORIGIN', 'DEST'], percent_tokeep = 80):
+        self.features_tofilter = features_tofilter
+        self.percent_tokeep = percent_tokeep
+        self.high_percentile = None
+        self.low_percentile = None
+    
+    def fit(self, df, labels=None): 
+        print('Fit high percentile filter...')
+        for feature_tofilter in self.features_tofilter:
+            # Get feature_tofilter values that represent 80% of data
+            self.high_percentile = ((((df[[feature_tofilter]].groupby(feature_tofilter).size() / len(df)).sort_values(ascending=False)) * 100).cumsum() < self.percent_tokeep).where(lambda x : x == True).dropna().index.values.tolist()
+            self.low_percentile = ((((df[[feature_tofilter]].groupby(feature_tofilter).size() / len(df)).sort_values(ascending=False)) * 100).cumsum() >= self.percent_tokeep).where(lambda x : x == True).dropna().index.values.tolist()
+
+            total = len(df[feature_tofilter].unique())
+            high_percentile_sum = len(self.high_percentile)
+            low_percentile_sum = len(self.low_percentile)
+            high_low_sum = high_percentile_sum + low_percentile_sum
+
+            print(f'Total number of {feature_tofilter} values : {total}')
+            print(f'Number of {feature_tofilter} high percentile (> {self.percent_tokeep}%) values : {high_percentile_sum}')
+            print(f'Number of {feature_tofilter} low percentile values : {low_percentile_sum}')
+            print(f'Sum of high percentile + low percentile values : {high_low_sum}')
+        
+        print('End of high percentile filter fit')
+        return self
+    
+    def transform(self, df):       
+        if (self.features_tofilter != None):
+            print('Apply high percentile filter...')
+            
+            for feature_tofilter in self.features_tofilter:
+                print(f'Apply filter on feature {feature_tofilter}')
+                # To do for later : apply low_percentile specific to the feature, and not only last calculated low_percentile  (in our case it's the same percentile for ORIGIN and DEST so this is not a problem)
+                df.loc[df[feature_tofilter].isin(self.low_percentile), feature_tofilter] = 'OTHERS'   
+            
+            return(df)    
+
+        else:
+            return(df)
+        
+
+    
+class DenseToSparseConverter(BaseEstimator, TransformerMixin):
+    def __init__(self):  # If None : every column is kept, nothing is done
+        return None
+    
+    def fit(self, matrix, labels=None):      
+        return self
+    
+    def transform(self, matrix):   
+        return(sparse.csr_matrix(matrix))
+
+    
+'''
+This class adds polynomial features in univariate way  (if feature X and n_degree 3 :  then it will add X², X³, and an intercept at the end)
+
+Requires ndarray as input
+'''    
+class PolynomialFeaturesUnivariateAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, n_degrees=2):
+        self.n_degrees = n_degrees
+        self.fitted = False
+    
+    def fit(self, df, labels=None):
+        self.fitted = True
+        return self
+    
+    def transform(self, df):
+        if (self.fitted == False):
+            self.fit(df)
+
+        nb_instances, n_features = df.shape
+        df_poly = np.empty((nb_instances, 0)) # Create empty array of nb_instances line and 0 features yet (we'll concatenate polynomial features to it)
+
+        progbar = tqdm(range(n_features))
+        print('Adding polynomial features')
+
+        for feature_index in range(n_features):    
+            df_1feature = df[:,feature_index]  # Reshape 
+
+            for n_degree in range(self.n_degrees):
+                df_poly = np.c_[df_poly, np.power(df_1feature, n_degree + 1)]
+
+            progbar.update(1)
+
+        # Add bias (intercept)
+        df_poly = np.c_[df_poly, np.ones((len(df_poly), 1))]  # add x0 = 1 feature        
+        
+        return(df_poly)
+
+'''
+This class adds polynomial features in univariate way  (if feature X and n_degree 3 :  then it will add X², X³, and an intercept at the end)
+
+Requires DataFrame as input
+'''        
+    
+class PolynomialFeaturesUnivariateAdder_DataFrame(BaseEstimator, TransformerMixin):
+    def __init__(self, n_degrees=2):
+        self.n_degrees = n_degrees
+        self.fitted = False
+    
+    def fit(self, df, features_toadd=None):  
+        print('fit')
+        self.features_toadd = features_toadd
+        print('Features to add :')
+        print(self.features_toadd)
+        self.fitted = True
+        return self
+    
+    def transform(self, df):
+        print('transform')
+        if (self.fitted == False):
+            self.fit(df)
+
+        nb_instances, n_features = df.shape
+        #df_poly = np.empty((nb_instances, 0)) # Create empty array of nb_instances line and 0 features yet (we'll concatenate polynomial features to it)
+        #df_poly = pd.DataFrame(index=df.index,columns=None)
+        
+        progbar = tqdm(range(len(self.features_toadd)))
+        print('Adding polynomial features')
+
+        for column_name in self.features_toadd:    
+            #df_1feature = df.loc[:, column_name]
+
+            for n_degree in range(1, self.n_degrees):
+                #df = pd.concat([df, np.power(df.loc[:, column_name], n_degree + 1)], axis=1)
+                df = pd.concat([df, np.power(df[[column_name]], n_degree + 1).rename(columns={column_name : column_name+'_DEG'+str(n_degree+1)})], axis=1)
+
+            progbar.update(1)
+
+        # Add bias (intercept)
+        #df_poly = pd.concat([df_poly, np.ones((len(df_poly), 1))])  # add x0 = 1 feature        
+        #del df
+        return(df)    
+    
+    
+class StandardScalerMultiple_old(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.fitted = False
+    
+    def fit(self, df, columns=None):              
+        self.columns = columns
+        self.scaler = StandardScaler()
+  
+        if (self.columns == None):
+            return(df)
+        else:
+            self.scaler.fit(df[self.columns].to_numpy())            
+        
+        return self
+    
+    def transform(self, df):
+        if (self.fitted == False):
+            self.fit(df)
+        
+        if (self.columns == None):
+            return(df)
+        
+        else:
+            df[self.columns] = self.scaler.transform(df[self.columns].to_numpy())
+
+        return(df)
+        
+class StandardScalerMultiple(BaseEstimator, TransformerMixin):
+    def __init__(self, features_toscale=None):
+        self.fitted = False
+        self.columns = features_toscale
+    
+    def fit(self, df):              
+        print('Fit Std scale multiple')
+        self.scaler = StandardScaler()
+  
+        if (self.columns == None):
+            self.fitted = True
+            return(df)
+        else:
+            self.scaler.fit(df[self.columns].to_numpy())            
+            self.fitted = True
+        
+        return self
+    
+    def transform(self, df):
+        print('Transform Std scale multiple')
+        if (self.fitted == False):
+            self.fit(df)
+        
+        if (self.columns == None):
+            return(df)
+        
+        else:
+            df.loc[:, self.columns] = self.scaler.transform(df.loc[:, self.columns].to_numpy())
+
+        return(df)
+        
+
+class MinMaxScalerMultiple(BaseEstimator, TransformerMixin):
+    def __init__(self, features_toscale=None):
+        self.fitted = False
+        self.columns = features_toscale
+    
+    def fit(self, df):              
+        print('Fit Min max scaler multiple')
+        self.scaler = MinMaxScaler()
+  
+        if (self.columns == None):
+            self.fitted = True
+            return(df)
+        else:
+            self.scaler.fit(df[self.columns].to_numpy())            
+            self.fitted = True
+        
+        return self
+    
+    def transform(self, df):
+        print('Transform Min max scaler multiple')
+        if (self.fitted == False):
+            self.fit(df)
+        
+        if (self.columns == None):
+            return(df)
+        
+        else:
+            df.loc[:, self.columns] = self.scaler.transform(df.loc[:, self.columns].to_numpy())
+
+        return(df)        
+        
+class LogTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, features_totransform = None):  # If None : every column is kept, nothing is done
+        self.features_toselect = features_toselect
+    
+    def fit(self, df, labels=None):      
+        return self
+    
+    def transform(self, df):       
+        if (self.features_toselect != None):
+            filter_cols = [col for col in df if (col.startswith(tuple(self.features_toselect)))]
+            
+            filter_cols.sort()
+            
+            print("Features selected (in order): " + str(df[filter_cols].columns))
+            
+            df = df.loc[:, filter_cols]
+            return(df)
+
+        else:
+            return(df)
+ 
+        
